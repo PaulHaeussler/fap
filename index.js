11c5e56a0b39cff15c3055af819e3e16556f7155
Paul <paulhaeussler@gmx.de>
2021-01-23 02:17:52 +0100

    1

--- index.js
+++ index.js
@@ -35,24 +35,24 @@ app.use('/', express.static(__dirname));
 app.get('/', cors(copts), function(req, res){
     logIP(req.ip);
     //validate
-    res.sendFile("public/index.html");
-    //res.sendFile("public/start.html");
+    res.sendFile("public/index.html", {root: __dirname});
+    //res.sendFile("public/start.html", {root: __dirname});
 });
 app.get('/done', cors(copts), function(req, res){
     logIP(req.ip);
-    res.sendFile("private/start.html");
+    res.sendFile("private/start.html", {root: __dirname});
 });
 app.get('/overview', cors(copts), function(req, res){
     logIP(req.ip);
-    res.sendFile("private/overview.html");
+    res.sendFile("private/overview.html", {root: __dirname});
 });
 app.get('/login', cors(copts), function(req, res){
     logIP(req.ip);
-    res.sendFile("auth/login.html");
+    res.sendFile("auth/login.html", {root: __dirname});
 });
 app.get('/register', cors(copts), function(req, res){
     logIP(req.ip);
-    res.sendFile("auth/register.html");
+    res.sendFile("auth/register.html", {root: __dirname});
 });
 
 function logIP(ip){
